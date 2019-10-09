d7a0bdf333ea470343b02776eca341f2a85f8de4
Arjun Lather <arjunslather@gmail.com>
2019-10-09 23:11:49 +0530

    Add ParkingSlotService interface

---
+++ src/main/java/com/gojek/parkinglot/service/ParkingSlotService.java
@@ -0,0 +1,21 @@
+package com.gojek.parkinglot.service;
+
+import com.gojek.parkinglot.model.ParkingSlot;
+import com.gojek.parkinglot.model.Vehicle;
+
+import java.util.List;
+
+public interface ParkingSlotService {
+
+    void initParkingSlot(int slotSize);
+
+    int parkVehicle(Vehicle vehicle);
+
+    boolean unParkVehicle(int slotNumber);
+
+    List<Integer> getSlotPositionsForVehicleColor(String vehicleColor);
+
+    int getSlotNumberForRegNumber(final String regNumber);
+
+    List<ParkingSlot> getParkingStatus();
+}
